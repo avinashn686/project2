255d1f48d9be79ef75edcbe963c65c12392a953b
avinashn686 <48235777+avinashn686@users.noreply.github.com>
2019-04-08 15:59:34 +0530

    Add files via upload

--- globaltrade/spiders/trade.py
+++ globaltrade/spiders/trade.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
-
+from ..items import GlobaltradeItem
 
 class TradeSpider(scrapy.Spider):
     name = 'trade'
     
     start_urls = ['https://www.globaltrade.net/expert-service-provider.html']
+
+    
     page_number= 2
-    u=0
+    
     def parse(self, response):
-        # a=''
+        
         next_page="https://www.globaltrade.net/United-States/expert-service-provider.html"
         
         yield response.follow(next_page, callback = self.page2)
@@ -26,65 +27,44 @@ class TradeSpider(scrapy.Spider):
             s='https://www.globaltrade.net'
             news=''
             news=s+i
+            
+            
             yield response.follow(news,callback=self.output)
             
         pages='https://www.globaltrade.net/United-States/expert-service-provider.html?pageSize=10&orderBy=1&filterByPost=false&filterByRef=false&topicClear=false&industryClear=false&currentPage='+ str(TradeSpider.page_number) 
         if TradeSpider.page_number <= 401:
           TradeSpider.page_number+=1
           yield response.follow(pages, callback=self.page2)
-
-
-
-
-
-
-
-
     def output(self,response):
-        logo=''
-        titles=''
-        sub_title=''
-        primary_location=''
-        area_of_expertise=''
-        about=''
-        website=''
-        languages_spoken=''
-        page_url=''
-
-        image=[]
+        item=GlobaltradeItem()
+        
         image=response.css('div.image').css('img ::attr(data-original)').extract()
         
-        logo='logourl:'+str(image[0])
-        
+        item['logo']=image
         titles=response.css('h1.sp-title').css('h1 ::text').extract()
-        title='title:'+str(titles[0])
+        
+        item['title']=titles
         
         sub_titles=response.css('span.sub').css('span ::text').extract()
-        sub_title='sub_title:'+str(sub_titles[0])
+        
+        item['sub_title']=sub_titles
         
         rows=response.css('div.profile-details  tr')
-
-
-
-
-
-        
-
-
-        
         for row in rows:
             primary_locations=[]
             primary_locations.append(response.css('td').css('td ::text').extract())
         
         e=primary_locations[0]
         
-        filename = 'trial%d.json'%TradeSpider.u 
+        for j in e:
+            if j=='\n':
+                e.remove(j)
+            j.strip('\n')
+        item['table']=e
+        
+        
+        filename = 'results.json' 
         with open(filename, 'a+') as f:
-        
-              f.writelines("%s\n"%logo)
-              f.writelines("%s\n"%title)
-              f.writelines("%s\n"%sub_title)
-              f.writelines(e)
+            f.writelines(str(item))
+            f.write('\n')
         self.log('Saved file %s' % filename)
-        TradeSpider.u+=1
-        
